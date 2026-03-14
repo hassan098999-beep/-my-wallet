e7e13a17732ba83bbb0172ab73fe482c62211dac
hassan098999-beep <hassan098999-beep@users.noreply.github.com>
2026-03-14 03:06:14 +0000

    deploy: 3b6f6ee36fafce7935b36ccf35ad45346341f167

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/-my-wallet/sw.js', { scope: '/-my-wallet/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
